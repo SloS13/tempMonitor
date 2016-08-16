519bc483370e8103b9b323662bd05ff2ea6875cc
Jess Maloney <jmaloney@trevetts.com>
2016-08-16 19:32:18 -0400

    F

--- test.php
+++ test.php
@@ -4,7 +4,9 @@ $output = shell_exec('/var/www/html/python/./getTemp');
 
 if ( substr_count($output,'YES') ) {
     $tempRaw = substr($output, -6);
-    echo 'Temp:'.$tempRaw;
+    $tempC = $tempRaw / 1000;
+    $tempF = ($tempC * 9 / 5) + 32;
+    echo 'Temp:'.$tempF;
 } else {
     echo 'ERROR';
 }
