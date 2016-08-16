88149276fa27513cd106c671171e05503e4171de
Jess Maloney <jmaloney@trevetts.com>
2016-08-16 19:30:00 -0400

    output

--- test.php
+++ test.php
@@ -4,6 +4,7 @@ $output = shell_exec('/var/www/html/python/./getTemp');
 
 if ( substr_count($output,'YES') ) {
     $tempRaw = substr($output, -5);
+    echo 'Temp:'.$tempRaw;
 } else {
     echo 'ERROR';
 }
