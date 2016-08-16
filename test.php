b6e904ffb4eb2d156cec6c9323818dbe3d5a370b
Jess Maloney <jmaloney@trevetts.com>
2016-08-16 19:30:41 -0400

    updating

--- test.php
+++ test.php
@@ -3,7 +3,7 @@
 $output = shell_exec('/var/www/html/python/./getTemp');
 
 if ( substr_count($output,'YES') ) {
-    $tempRaw = substr($output, -5);
+    $tempRaw = substr($output, -6);
     echo 'Temp:'.$tempRaw;
 } else {
     echo 'ERROR';
