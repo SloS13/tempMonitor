97caf77a06d3f118d64bae71f5d0d47338429d6b
ka24det@gmail.com <Einstein8430>
2016-08-20 17:37:13 -0400

    a

--- js/script.js
+++ js/script.js
@@ -180,16 +180,7 @@ function liveChart(){
 		data: [
 		{
 			type: "spline", //change it to line, area, column, pie, etc
-			dataPoints: [
-				{ x: 10, y: 10 },
-				{ x: 20, y: 12 },
-				{ x: 30, y: 8 },
-				{ x: 40, y: 14 },
-				{ x: 50, y: 6 },
-				{ x: 60, y: 24 },
-				{ x: 70, y: -4 },
-				{ x: 80, y: 10 }
-			]
+			dataPoints: recentTemps
 		}
 		]
 	};
